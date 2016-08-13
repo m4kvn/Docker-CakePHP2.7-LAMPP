f589608b4df630f100c59e3cb41f29126181f178
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-08-13 19:34:20 +0900

    docker stop 時のバックアップ処理を廃止

--- lamp/startup.sh
+++ lamp/startup.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
+mysql_install_db
 service mysql start
-
-trap_TERM() {
-    echo 'SIGTERM ACCEPTED.'
-    MSG=`/root/backupmysql.sh`
-    echo $MSG
-    exit 0
-}
-
-trap 'trap_TERM' TERM
-
-apache2-foreground&
-
-while :
-do
-    sleep 5
-done
+apache2-foreground
